d14cd8c40a9aa0f633b31b58a879a34eb7347a6c
Nikola <nikola.zitko@gmail.com>
2022-08-16 18:16:39 +0200

    Add checkGrade test

--- cypress/e2e/checkGrade.cy.js
+++ cypress/e2e/checkGrade.cy.js
@@ -1,6 +1,12 @@
 
 describe('As a student, check grade given for Zavrsni rad', () => {
-  it('checks if grade for Zavrsni rad was 5', () => {
+  it('checks if grade for Zavrsni rad is 5', () => {
+    cy.loginAsStudent();
 
+    cy.get('a').contains('Ocjene').click({ force: true });
+    cy.get('a').contains('ZAVRŠNI RAD').click();
+
+    // this test is supposed to fail until it gets inserted after successfully completing the thesis.
+    cy.get('td').should('have.class', 'column-grade').contains('5');
   })
 })
